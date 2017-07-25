5af93217909a371e5b7c33cf7eb1f129ee5efde3
ndejonge <ndejonge@gmail.com>
2017-07-25 16:57:55 +0200

    added all http stuff from example....

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <memory>
 #include <chrono>
 #include <iostream>
+#include <fstream>
+
 #include <thread>
 #include <deque>
 
@@ -118,9 +120,6 @@ namespace http_stock_replies
 } // namespace stock_replies
 
 
-class http_request_handler
-{
-};
 
 class http_header
 {
@@ -637,13 +636,156 @@ private:
 	}
 };
 
+namespace mime_types
+{
+	struct mapping
+	{
+		const char* extension;
+		const char* mime_type;
+	} 
+	
+	mappings[] =
+	{
+		{ "gif", "image/gif" },
+		{ "htm", "text/html" },
+		{ "html", "text/html" },
+		{ "jpg", "image/jpeg" },
+		{ "png", "image/png" }
+	};
+
+	std::string extension_to_type(const std::string& extension)
+	{
+		for (mapping m : mappings)
+		{
+			if (m.extension == extension)
+			{
+				return m.mime_type;
+			}
+		}
+
+		return "text/plain";
+	}
+} // namespace mime_types
+
+class http_request_handler
+{
+public:
+	http_request_handler(const http_request_handler&) = delete;
+	http_request_handler& operator=(const http_request_handler&) = delete;
+
+	/// Construct with a directory containing files to be served.
+	explicit http_request_handler(const std::string& doc_root) : doc_root_(doc_root)
+	{
+	}
+
+	/// Handle a request and produce a reply.
+	void handle_request(const http_request& request, http_reply& reply)
+	{
+		// Decode url to path.
+		std::string request_path;
+
+		if (!url_decode(request.uri, request_path))
+		{
+			reply = http_reply::stock_reply(http_reply::bad_request);
+			return;
+		}
+
+		// Request path must be absolute and not contain "..".
+		if (request_path.empty() || request_path[0] != '/'
+			|| request_path.find("..") != std::string::npos)
+		{
+			reply = http_reply::stock_reply(http_reply::bad_request);
+			return;
+		}
+
+		// If path ends in slash (i.e. is a directory) then add "index.html".
+		if (request_path[request_path.size() - 1] == '/')
+		{
+			request_path += "index.html";
+		}
+
+		// Determine the file extension.
+		std::size_t last_slash_pos = request_path.find_last_of("/");
+		std::size_t last_dot_pos = request_path.find_last_of(".");
+		std::string extension;
+		if (last_dot_pos != std::string::npos && last_dot_pos > last_slash_pos)
+		{
+			extension = request_path.substr(last_dot_pos + 1);
+		}
+
+		// Open the file to send back.
+		std::string full_path = doc_root_ + request_path;
+		std::ifstream is(full_path.c_str(), std::ios::in | std::ios::binary);
+		if (!is)
+		{
+			reply = http_reply::stock_reply(http_reply::not_found);
+			return;
+		}
+
+		// Fill out the reply to be sent to the client.
+		reply.status = http_reply::ok;
+		char buf[512];
+		while (is.read(buf, sizeof(buf)).gcount() > 0)
+			reply.content.append(buf, is.gcount());
+		reply.headers.resize(2);
+		reply.headers[0].name = "Content-Length";
+		reply.headers[0].value = std::to_string(reply.content.size());
+		reply.headers[1].name = "Content-Type";
+		reply.headers[1].value = mime_types::extension_to_type(extension);
+	}
+
+private:
+	/// The directory containing the files to be served.
+	std::string doc_root_;
+
+	/// Perform URL-decoding on a string. Returns false if the encoding was
+	/// invalid.
+	static bool url_decode(const std::string& in, std::string& out)
+	{
+		out.clear();
+		out.reserve(in.size());
+		for (std::size_t i = 0; i < in.size(); ++i)
+		{
+			if (in[i] == '%')
+			{
+				if (i + 3 <= in.size())
+				{
+					int value = 0;
+					std::istringstream is(in.substr(i + 1, 2));
+					if (is >> std::hex >> value)
+					{
+						out += static_cast<char>(value);
+						i += 2;
+					}
+					else
+					{
+						return false;
+					}
+				}
+				else
+				{
+					return false;
+				}
+			}
+			else if (in[i] == '+')
+			{
+				out += ' ';
+			}
+			else
+			{
+				out += in[i];
+			}
+		}
+		return true;
+	}
+};
 
 
 
 class http_client_connection_handler : public std::enable_shared_from_this<http_client_connection_handler>
 {
 public:
-	http_client_connection_handler(boost::asio::io_service& service) : service_(service), socket_(service), write_strand_(service)
+	http_client_connection_handler(boost::asio::io_service& service) : service_(service), socket_(service), write_strand_(service), http_request_handler_("C:\\temp")
 	{
 	}
 
