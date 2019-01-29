aa696f4359bbb434563294419d75cda0bad07720
ndejonge <ndejonge@gmail.com>
2019-01-29 19:53:38 +0100

    wip

--- http_upstream_node.h
+++ http_upstream_node.h
@@ -51,48 +51,54 @@ enum servertype_specialisations
 	for_nginx,
 	for_haproxy
 };
+template<class I>
+class upstream_node_controller;
 
 template<servertype_specialisations>
 class enable_server_as_upstream
 {
+public:
+	enable_server_as_upstream
+protected:
+	upstream_node_controller upstream_node_controller_;
 };
 
 //CRTP
 template<class I>
-class upstream_node_interface
+class upstream_node_controller
 {
 
 public:
 	const result add(std::string& myurl) const noexcept
 	{
-		return static_cast<T*>add_impl(myrl);
+		return static_cast<T*>(this)->add_impl(myrl);
 	}
 
 	const result remove(std::string& myurl) const noexcept
 	{
-		return static_cast<T*>remove_impl(myrl);
+		return static_cast<T*>(this)->remove_impl(myrl);
 	}
 
 	const result enable(std::string& myurl) const noexcept
 	{
-		return static_cast<T*>enable_impl(myrl);
+		return static_cast<T*>(this)->enable_impl(myrl);
 	}
 
 	const result disable(std::string& myurl) const noexcept
 	{
-		return static_cast<T*>enable_impl(myrl);
+		return static_cast<T*>(this)->enable_impl(myrl);
 	}
 
 	const std::string list(std::string& myurl) const noexcept
 	{
-		return static_cast<T*>enable_impl(myrl);
+		return static_cast<T*>(this)->enable_impl(myrl);
 	}
 };
 
 namespace implementations 
 {
 
-class nginx : public upstream_node_interface<nginx>
+class nginx : public upstream_node_controller<nginx>
 {
 public:
 	nginx(const std::string& nginx_endpoint = "http://localhost:4000/dynamic")
